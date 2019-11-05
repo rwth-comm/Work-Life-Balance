6c36a671317eabb766ee65330422563e40dc8a33
LukasSteinke <56429709+LukasSteinke@users.noreply.github.com>
2019-11-05 13:26:24 +0100

    Create AnalysisScript.R

---
+++ AnalysisScript.R
@@ -0,0 +1,10 @@
+# AnalysisScript.R
+## Teamnamen: Work-Life-Balance
+
+# Pakete laden: ----
+
+# Rohdaten laden: ----
+
+# DataCleaning und Skalenberechnung: ----
+
+# Statistische Analyse und Grafiken: ----
